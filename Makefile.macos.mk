f34c476efe967b572287600ef5d5a1a3366ed841
kbys <work4kbys@gmail.com>
2018-10-11 21:39:18 +0900

    add makefiles

---
+++ Makefile.macos.mk
@@ -0,0 +1,23 @@
+CC=gcc
+AR=ar
+CFLAGS=-Wall -O3 -F $(HOME)/Library/Frameworks
+INCLUDE_PATHS=
+
+BUILD_DIR=build/macos
+SOURCES = $(wildcard src/*.c)
+OBJECTS = $(addprefix $(BUILD_DIR)/, $(notdir $(SOURCES:.c=.o)))
+TARGET = $(BUILD_DIR)/libbiext.a
+
+all: $(BUILD_DIR) $(TARGET)
+
+$(BUILD_DIR):
+	mkdir -p $@
+
+$(BUILD_DIR)/%.o: src/%.c
+	$(CC) -c $^ -o $@ -I include $(CFLAGS) $(INCLUDE_PATHS)
+
+$(TARGET): $(OBJECTS)
+	$(AR) rcs $@ $^
+
+clean:
+	rm -rf $(BUILD_DIR)
---
+++ Makefile.mingw.mk
@@ -0,0 +1,23 @@
+CC=x86_64-w64-mingw32-gcc
+AR=x86_64-w64-mingw32-ar
+CFLAGS=-Wall -O3 `/usr/local/x86_64-w64-mingw32/bin/sdl2-config --cflags`
+INCLUDE_PATHS=-I /usr/local/x86_64-w64-mingw32/include
+
+BUILD_DIR=build/mingw
+SOURCES = $(wildcard src/*.c)
+OBJECTS = $(addprefix $(BUILD_DIR)/, $(notdir $(SOURCES:.c=.o)))
+TARGET = $(BUILD_DIR)/libbiext.a
+
+all: $(BUILD_DIR) $(TARGET)
+
+$(BUILD_DIR):
+	mkdir -p $@
+
+$(BUILD_DIR)/%.o: src/%.c
+	$(CC) -c $^ -o $@ -I include $(CFLAGS) $(INCLUDE_PATHS)
+
+$(TARGET): $(OBJECTS)
+	$(AR) rcs $@ $^
+
+clean:
+	rm -rf $(BUILD_DIR)
