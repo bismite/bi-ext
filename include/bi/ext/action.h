5b9b46b7f58b8917bf7e522a96afb86a641a8ac1
kbys <work4kbys@gmail.com>
2018-10-29 15:22:35 +0900

    add action callback

--- include/bi/ext/action.h
+++ include/bi/ext/action.h
@@ -8,6 +8,7 @@ typedef struct _BiAction BiAction;
 
 typedef void (*bi_action_start_function)(BiNode*,BiAction*,double); // target-node,action,now
 typedef bool (*bi_action_update_function)(BiNode*,BiAction*,double); // target-node,action,rate
+typedef void (*bi_action_on_finish_callback)(BiAction*,void*); // action,context
 
 struct _BiAction {
   bi_action_start_function _start;
@@ -17,13 +18,15 @@ struct _BiAction {
   double duration;
   void* action_data;
   BiTimer* _timer;
+  bi_action_on_finish_callback on_finish;
+  void* on_finish_callback_context;
 };
 
 extern void bi_action_start(BiNode *node, BiAction *action,double now);
 extern void bi_action_update(BiNode *node, BiAction *action, double rate);
 
 extern void bi_add_action(BiContext* context,BiNode* node,BiAction* action);
-extern void bi_remove_action(BiContext* context,BiNode* node,BiAction* action);
+extern void bi_remove_action(BiContext* context,BiAction* action);
 
 //
 // actions
--- src/action.c
+++ src/action.c
@@ -18,28 +18,34 @@ void bi_action_update(BiNode *node, BiAction *action, double rate)
 
 static bool do_actions(BiContext* context,BiNode* node,double now,BiTimer* timer)
 {
+  bool finish = false;
   ActionDoer* doer = timer->userdata;
   BiAction* a = doer->action;
   double rate = (now - a->start_at) / a->duration;
   if(rate<0.0) rate = 0.0;
-  if(rate>1.0) rate = 1.0;
+  if(rate>1.0){
+    rate = 1.0;
+    finish = true;
+  }
   bi_action_update(node,a,rate);
+  if( finish && a->on_finish ) {
+    a->on_finish(a,a->on_finish_callback_context);
+  }
   return true;
 }
 
 void bi_add_action(BiContext* context,BiNode* node,BiAction* action)
 {
-  action->_timer = malloc(sizeof(BiTimer));
-  bi_timer_init(action->_timer, node, do_actions, 0, -1, NULL);
+  action->_timer = bi_timer_alloc(node, do_actions, 0, -1, NULL);
   ActionDoer* doer = malloc(sizeof(ActionDoer));
   doer->action = action;
   action->_timer->userdata = doer;
   bi_add_timer(context,action->_timer);
 }
 
-void bi_remove_action(BiContext* context,BiNode* node,BiAction* action)
+void bi_remove_action(BiContext* context,BiAction* action)
 {
-  bi_remove_timer(context,action->_timer);
+  bi_finish_timer(context,action->_timer);
   free(action->_timer->userdata);
   action->_timer = NULL;
 }
